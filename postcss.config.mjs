0c6fd82229c62b40ef060b6172a1be0da1d26fc2
Amit Chawla <129105171+amitstar69@users.noreply.github.com>
2025-11-24 14:05:22 -0500

    Update postcss.config.mjsUpdate postcss.config.mjs to use @tailwindcss/postcss plugin

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,11 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+const config = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
 };
+
+export default config;
