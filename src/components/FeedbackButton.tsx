d8b86b1266a8bd917cc1cfa7b349fc66559ea070
Amit Chawla <129105171+amitstar69@users.noreply.github.com>
2026-01-17 09:46:23 -0500

    Update Formspree URL in FeedbackButton component

--- src/components/FeedbackButton.tsx
+++ src/components/FeedbackButton.tsx
@@ -14,7 +14,7 @@
       setIsSubmitting(true);
 
       try {
-        const response = await fetch('https://formspree.io/f/xanyrddo', {
+        const response = await fetch('https://formspree.io/f/mgooodoz', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
